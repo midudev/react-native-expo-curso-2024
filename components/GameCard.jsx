a460188a964db8023642ba7b54973079aca8958f
Miguel Ángel Durán <miduga@gmail.com>
2024-07-18 19:43:44 +0200

    Add code

--- components/GameCard.jsx
+++ components/GameCard.jsx
@@ -1,14 +1,36 @@
 import { useEffect, useRef } from "react";
-import { View, StyleSheet, Text, Image, Animated } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  Animated,
+  Pressable,
+} from "react-native";
+import { Score } from "./Score";
+import { Link } from "expo-router";
+import { styled } from "nativewind";
+
+const StyledPressable = styled(Pressable);
 
 export function GameCard({ game }) {
   return (
-    <View key={game.slug} style={styles.card}>
-      <Image source={{ uri: game.image }} style={styles.image} />
-      <Text style={styles.title}>{game.title}</Text>
-      <Text style={styles.score}>{game.score}</Text>
-      <Text style={styles.description}>{game.description}</Text>
-    </View>
+    <Link href={`/${game.slug}`} asChild>
+      <StyledPressable className="active:opacity-70 border border-black active:border-white/50 mb-2 bg-gray-500/10 rounded-xl p-4">
+        <View className="flex-row gap-4" key={game.slug}>
+          <Image source={{ uri: game.image }} style={styles.image} />
+          <View className="flex-shrink">
+            <Text className="mb-1" style={styles.title}>
+              {game.title}
+            </Text>
+            <Score score={game.score} maxScore={100} />
+            <Text className="mt-2 flex-shrink" style={styles.description}>
+              {game.description.slice(0, 100)}...
+            </Text>
+          </View>
+        </View>
+      </StyledPressable>
+    </Link>
   );
 }
 
---
+++ components/Icons.jsx
@@ -0,0 +1,14 @@
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+export const CircleInfoIcon = (props) => (
+  <FontAwesome6 name="circle-info" size={24} color="white" {...props} />
+);
+
+export const HomeIcon = (props) => (
+  <FontAwesome name="home" size={32} color="white" {...props} />
+);
+
+export const InfoIcon = (props) => (
+  <FontAwesome name="info" size={32} color="white" {...props} />
+);
